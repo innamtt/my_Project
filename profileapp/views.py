53a4330310a14bc6fd245648c0094e6e73616113
innamtt <innamtt@gmail.com>
2021-07-29 11:29:06 +0900

    2021-07-29 1st Commit

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,10 +1,14 @@
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
+from profileapp.decorators import profile_ownership_required
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
 
 
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'post')
 class ProfileCreateView(CreateView):
     model = Profile
     form_class = ProfileCreationForm
